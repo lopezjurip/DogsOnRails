288f9754cf053f6cb0365f655dab8f6a29021525
Patricio López <lopezjuripatricio@gmail.com>
2015-04-19 10:59:04 -0300

    Refinada migración de User y modelo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string           not null
+#  email      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class User < ActiveRecord::Base
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true, format: /\A[a-z0-9]+[a-z0-9\._-]*@[a-z0-9\.]+\.[a-z]{2,5}\z/i
 end

aecc33eb71bb004e880390bce66732f97f90af6a
Patricio López <lopezjuripatricio@gmail.com>
2015-04-20 22:43:22 -0300

    Destroy dependency en los Dogs de User

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,5 +13,5 @@ class User < ActiveRecord::Base
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true, format: /\A[a-z0-9]+[a-z0-9\._-]*@[a-z0-9\.]+\.[a-z]{2,5}\z/i
 
-  has_many :dogs
+  has_many :dogs, dependent: :destroy # Will destroy all user's owned dogs
 end

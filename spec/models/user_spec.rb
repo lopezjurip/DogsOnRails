9118d9663e5ac83aea0a30a40e2bf06efbb7db22
Patricio López <lopezjuripatricio@gmail.com>
2015-04-22 12:11:02 -0300

    Agregados test para User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'does not allow empty email' do
+    user = User.new(name: "Patricio")
+    expect(user).to be_invalid
+  end
+
+  it 'does not allow invalid email' do
+    user = User.new(name: 'Patricio', email: 'this is not an email address')
+    expect(user).to be_invalid
+  end
+
+  it 'allow valid email' do
+    user = User.new(name: 'Patricio', email: 'patricio@email.com')
+    expect(user).to be_valid
+  end
+
+  it 'is invalid with a duplicate email address' do
+    User.create(name: 'Joe', email: 'tester@example.com')
+    user = User.new(name: 'Jane', email: 'tester@example.com')
+    expect(user).to be_invalid
+  end
 end

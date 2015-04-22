c376927c86a8947f144c45d4a43711019aace68b
Patricio López <lopezjuripatricio@gmail.com>
2015-04-22 14:52:32 -0300

    Mejor organización de los RSpec de User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,24 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'does not allow empty email' do
-    user = User.new(name: "Patricio")
-    expect(user).to be_invalid
-  end
+  describe 'creates user entry' do
+    context 'invalid email address' do
+      it 'empty email field' do
+        user = User.new(name: 'Patricio')
+        expect(user).to be_invalid
+      end
 
-  it 'does not allow invalid email' do
-    user = User.new(name: 'Patricio', email: 'this is not an email address')
-    expect(user).to be_invalid
-  end
+      it 'email address does not match REGEX' do
+        user = User.new(name: 'Patricio', email: 'this is not an email address')
+        expect(user).to be_invalid
+      end
 
-  it 'allow valid email' do
-    user = User.new(name: 'Patricio', email: 'patricio@email.com')
-    expect(user).to be_valid
-  end
+      it 'a duplicate email address exist' do
+        User.create(name: 'Joe', email: 'tester@example.com')
+        user = User.new(name: 'Jane', email: 'tester@example.com')
+        expect(user).to be_invalid
+      end
+    end
 
-  it 'is invalid with a duplicate email address' do
-    User.create(name: 'Joe', email: 'tester@example.com')
-    user = User.new(name: 'Jane', email: 'tester@example.com')
-    expect(user).to be_invalid
+    context 'enters valid parameters' do
+      it 'allow valid email' do
+        user = User.new(name: 'Patricio', email: 'patricio@email.com')
+        expect(user).to be_valid
+      end
+    end
   end
 end
+

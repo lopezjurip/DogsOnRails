fa54cb3a025a1d334444979ce8f0edc4de79792f
Patricio López <lopezjuripatricio@gmail.com>
2015-04-22 11:48:25 -0300

    Agregados test para Dog

--- spec/models/dog_spec.rb
+++ spec/models/dog_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Dog, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'can be instantiated' do
+    # https://www.relishapp.com/rspec/rspec-expectations/docs/built-in-matchers
+
+    dog = Dog.new(name: 'Cachupín')
+    expect(dog.name).to eq('Cachupín')
+    expect(dog.male).not_to be_nil
+    expect(dog.owner).to be_nil
+    expect(dog.veterinaries).to be_empty
+  end
 end
